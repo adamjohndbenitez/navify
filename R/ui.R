3eebd9518a29d04c6ead3708a5ec7bc43167678d
Adam John Benitez <adamjohndbenitez72891@gmail.com>
2016-07-14 22:49:53 +0800

    [Task2] v2 - Add Location & Destination SelectItems.

--- R/ui.R
+++ R/ui.R
@@ -8,10 +8,20 @@
 library(shiny)
 library(shinydashboard)
 
+streets <- read.csv(file = "C:\\Users\\Adam\\Documents\\CIT-U\\Masters of Computer Science\\Capstone - Thesis\\Navify - Traffixer\\Navify\\street.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
 shinyUI(fluidPage(
   dashboardPage(
     dashboardHeader(title = "Navify"),
-    dashboardSidebar(),
+    dashboardSidebar(
+      # Select location
+      selectInput(inputId = "locationId", label = "Select Location",
+                  choices = streets$name),
+
+      # Select destination
+      selectInput(inputId = "destinationId", label = "Select Destination",
+                  choices = streets$name)
+    ),
     dashboardBody()
   )
 ))
